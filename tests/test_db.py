428ec92c30a5e7f1d3dfd91312e234d70ccf003a
Egor Baturin <egoriyaa@github.com>
2023-12-29 01:43:26 +0300

    minor fix

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,8 +7,6 @@ db_user = os.getenv("POSTGRES_USER")
 db_password = os.getenv("POSTGRES_PASSWORD")
 db_name = os.getenv("POSTGRES_DB")
 table_name = os.getenv("POSTGRES_TABLE")
-db_host = os.getenv("DB_HOST")
-db_port = int(os.getenv("DB_PORT"))
 query_drop_table = f"DROP TABLE IF EXISTS {table_name}"
 
 
@@ -37,7 +35,7 @@ def test_insert_data_into_table():
 
 
 @pytest.mark.parametrize("image_hash, expected_result", [("123", "1"), ("1234", False)])
-def test_insert_data_into_table(image_hash, expected_result):
+def test_find_picture_in_table(image_hash, expected_result):
     db = DataBase(db_name=db_name, db_user=db_user, db_password=db_password)
     db.execute_query(query_drop_table)
     db.create_table()
